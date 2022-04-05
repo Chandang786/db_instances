e65871d27e7a7796375fc6fc763d4ef3dfe0b204
Chandan Gulati <chandangulati001@gmail.com>
2022-04-05 12:40:35 +0530

    AWS providers added

--- providers.tf
+++ providers.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.8.0"
+    }
+  }
+}
 
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAQDRYIGWXOLQT7DJ3"
+  secret_key = "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"
+}
