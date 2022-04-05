6917aba946480d71fc736dcea2310fe38d25cf52
Chandan Gulati <chandangulati001@gmail.com>
2022-04-05 13:08:57 +0530

    RDS instances provisoning

---
+++ db_instance.tf
@@ -0,0 +1,13 @@
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "Passion@12"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
