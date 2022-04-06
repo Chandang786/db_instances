32137f078b9876ed78751593e82e6e858df3daa0
Chandan Gulati <chandangulati001@gmail.com>
2022-04-06 20:21:29 +0530

    Hide access and secrete key

--- providers.tf
+++ providers.tf
@@ -1,14 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.8.0"
     }
+
   }
 }
 
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAQDRYIGWXOLQT7DJ3"
-  secret_key = "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"
+  access_key = var.accesskey
+  secret_key = var.secret_key
 }
