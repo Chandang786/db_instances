63ad5eaa4b1216265e8b2348253ae363ec0949b1
Chandan Gulati <chandangulati001@gmail.com>
2022-04-06 20:20:39 +0530

    Added RDS module

--- db_instance.tf
+++ db_instance.tf
@@ -1,40 +1,34 @@
 
-
-resource "aws_db_instance" "Test_DB" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  db_name                 = "mydb"
-  username             = "admin"
-  password             = "Passion@12"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.RDS_MYSQL.id]
+module "MYSQL_RDS" {
+  source            = "../../modules/"
+  db_name           = "Database1"
+  user_name         = "master"
+  allocated_storage = 10
+  security_group_id = [aws_security_group.RDS_MYSQL.id]
 }
 
 variable "RDS_port" {
-  type = list
-  default = [443,3306,80]
+  type    = list(any)
+  default = [443, 3306, 80]
 }
 
 resource "aws_security_group" "RDS_MYSQL" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
- 
 
-dynamic "ingress" {
+
+  dynamic "ingress" {
 
     for_each = var.RDS_port
-  content {
-    
-    from_port        = ingress.value
-    to_port          = ingress.value
-    protocol         = "HTTPS"
-    cidr_blocks      = ["0.0.0.0/24"]
-    
+    content {
+
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "HTTPS"
+      cidr_blocks = ["0.0.0.0/24"]
+
+    }
   }
-}
   egress {
     from_port        = 0
     to_port          = 0
